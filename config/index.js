096dc98a6b517735b25d4c6e35ec4bccc53df535
Kifaa26 <akifahpalekar@gmail.com>
2024-08-08 14:13:09 +0200

    middleware

--- config/index.js
+++ config/index.js
@@ -1,16 +1,15 @@
 import { createPool } from "mysql2";
 import  'dotenv/config';
-config()
 let connection = createPool ({
     host: process.env.hostDb,
     user: process.env.userDb,
     password: process.env.pwdDb,
     database: process.env.dbName,
     multipleStatements: true,
-    connectionLimit: 30
+    connectionLimit: 30,
 })
-connection.on('connection', (err) => {
-    if(err) throw new Error ('Couldn\'t connect to the database, please try again later')
+connection.on('connection', (pool) => {
+    if(!pool) throw new Error ('Couldn\'t connect to the database, please try again later')
 })
 export {
     connection
