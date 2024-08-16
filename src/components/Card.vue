606aa209155a073382a4bccd52b531fdbcd4dccb
Kifaa26 <akifahpalekar@gmail.com>
2024-08-16 14:43:20 +0200

    Card slot

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+ <div class="card">
+        <div class="card-header">
+            <slot name="card-header"></slot>
+        </div>
+        <div class="card-body">
+            <slot name="card-body"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+name: 'CardComp'
+}
+</script>
+
+<style scoped>
+
+</style>
